6900ad8775b4fc070e1d18f2509a481b0a66e3de
Bão Bão <ducbaonguyen101098@gmail.com>
2020-04-17 11:58:00 +0700

    up

---
+++ body/digital-marketing/contact.php
@@ -0,0 +1,63 @@
+<div class="page-wrapper">
+    <div class="container-fluid p-0 pt-0">
+        <div class="page-content pt-0">
+            <!-- sub menu -->
+            <div class="sub-header border-bottom pl-4 pt-3 pb-3 bg-white">
+                <div class="d-flex no-block">
+                    <div class="align-self-center font-14 mr-3">
+                        <a href="./" class="" style="color: rgba(79,80,80,.7);">Phân tích</a>
+                    </div>
+                    <div class="align-self-center mr-3">
+                        <i class="fas fa-chevron-right text-muted"></i>
+                    </div>
+                    <div class="align-self-center font-14 mr-3" > <a style="color: rgba(79,80,80,.7);"
+                            href="<?=$rootURL?>">Marketing</a>
+                    </div>
+                    <div class="align-self-center mr-3">
+                        <i class="fas fa-chevron-right text-muted"></i>
+                    </div>
+                    <div class="align-self-center font-14" style="color: rgba(79,80,80,.7);"> <a 
+                            href="<?=$rootURL?>">Contact</a> </div>
+
+                </div>
+
+            </div>
+            <!-- sub menu --> 
+            <div class="row mt-5 justify-content-center"> 
+                <div class="col-12 text-center" style="max-width: 700px">
+                    <div class="pb-5">
+                        <div class="bg-white px-3 py-5 rounded shadow-sm">
+                            <div class="mb-5">
+                                <div class="font-gg font-weight-bold font-18">
+                                    Bổ xung thông tin
+                                </div>
+                                <div class="mt-1 font-gg font-weight-500 font-14 text-muted">
+                                    Vui lòng bổ xung thông tin để tiếp tục
+                                </div>
+                            </div> 
+                            <div class="mb-5"> 
+                                <div class="text-left d-flex no-block flex-column mb-4">
+                                    <div class="align-self-center font-gg font-weight-bold mb-2 font-13 w-100">Tên liên hệ</div>
+                                    <input id="input-name" value="" type="text" class="font-13 text-muted form-control" placeholder="Nhập tên liên hệ">
+                                </div> 
+                                <div class="text-left d-flex no-block flex-column mb-4">
+                                    <div class="align-self-center font-gg font-weight-bold mb-2 font-13 w-100">Số điện thoại</div>
+                                    <input id="input-phone" value="" type="text" class="font-13 text-muted form-control" placeholder="Nhập số điện thoại">
+                                </div>  
+                                <div class="text-left d-flex no-block flex-column">
+                                    <div class="align-self-center font-gg font-weight-bold mb-2 font-13 w-100">Email</div>
+                                    <input id="input-email" value="" type="text" class="font-13 text-muted form-control" placeholder="Nhập email">
+                                </div> 
+                            </div>
+                            <div class="text-right">
+                                <button class="btn-next btn btn-info font-gg font-15 font-weight-500 px-5">Tiếp tục</button>
+                            </div>
+                        </div>
+                    </div>
+                </div> 
+            </div> 
+        </div>
+    </div>
+</div>
+<script src="<?=$rootURL?>/dist/js/pages/my-function/init-function.js"></script>
+<script src="<?=$rootURL?>/dist/js/pages/digital-marketing/contact.js"></script>
---
+++ body/digital-marketing/google.php
@@ -0,0 +1,67 @@
+<div class="page-wrapper">
+    <div class="container-fluid p-0 pt-0">
+        <div class="page-content pt-0">
+            <!-- sub menu -->
+            <div class="sub-header border-bottom pl-4 pt-3 pb-3 bg-white">
+                <div class="d-flex no-block">
+                    <div class="align-self-center font-14 mr-3">
+                        <a href="./" class="" style="color: rgba(79,80,80,.7);">Phân tích</a>
+                    </div>
+                    <div class="align-self-center mr-3">
+                        <i class="fas fa-chevron-right text-muted"></i>
+                    </div>
+                    <div class="align-self-center font-14 mr-3" > <a style="color: rgba(79,80,80,.7);"
+                            href="<?=$rootURL?>">Marketing</a>
+                    </div>
+                    <div class="align-self-center mr-3">
+                        <i class="fas fa-chevron-right text-muted"></i>
+                    </div>
+                    <div class="align-self-center font-14 mr-3"> <a style="color: rgba(79,80,80,.7);"
+                            href="<?=$rootURL?>">Contact</a> </div>
+                    <div class="align-self-center mr-3">
+                    <i class="fas fa-chevron-right text-muted"></i>
+                    </div>
+                    <div class="align-self-center font-14 mr-3"> <a style="color: rgba(79,80,80,.7);" 
+                            href="<?=$rootURL?>">Widget</a> 
+                    </div>
+                    <i class="fas fa-chevron-right text-muted"></i>
+                    </div>
+                    <div class="align-self-center font-14 mr-3" style="color: rgba(79,80,80,.7);"> <a 
+                            href="<?=$rootURL?>">Google Ads</a> 
+                    </div>
+
+                </div>
+
+            </div>
+            <!-- sub menu --> 
+            <div class="row mt-5 justify-content-center"> 
+                <div class="col-12 text-center" style="max-width: 800px">
+                    <div class="pb-5">
+                        <div class="bg-white px-3 py-5 rounded shadow-sm">
+                            <div class="mb-5">
+                                <div class="font-gg font-weight-bold font-18">
+                                    Google Ads
+                                </div>
+                                <div class="mt-1 font-gg font-weight-500 font-14 text-muted">
+                                    Đưa website của bạn lên top Google Search nhanh chóng, bạn chủ động quản lý vị trí quảng cáo và chi phí chi trả cho Google
+                                </div>
+                            </div> 
+                            <div class="mb-3 w-100" style="height: 400px"> 
+                            
+                            </div>
+                            <div class="mb-4 bg-dark-2 px-3 py-2 text-left">
+                                <div class="font-gg font-weight-500 font-14">Google Ads là công cụ quảng cáo của Google, Google chỉ tính chi phí khi khách hàng bấm chuột vào quảng cáo.</div>
+                            </div>
+                            <div class="">
+                                <button class="btn-skip btn btn-info font-gg font-15 font-weight-500 px-4 mr-3">Bỏ qua công cụ này</button>
+                                <button class="btn-use btn btn-danger font-gg font-15 font-weight-500 px-4">Dùng công cụ này</button>
+                            </div>
+                        </div>
+                    </div>
+                </div> 
+            </div> 
+        </div>
+    </div>
+</div>
+<script src="<?=$rootURL?>/dist/js/pages/my-function/init-function.js"></script>
+<script src="<?=$rootURL?>/dist/js/pages/digital-marketing/google.js"></script>  
