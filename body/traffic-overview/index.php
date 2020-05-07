70bad16f5967758a9c2bf6ad12012207e6598ddc
Bão Bão <ducbaonguyen101098@gmail.com>
2020-05-07 18:03:49 +0700

    up

---
+++ body/traffic-overview/index.php
@@ -0,0 +1,129 @@
+<div class="page-wrapper">
+    <div class="container-fluid p-0 pt-0">
+        <div class="page-content pt-0">
+            <!-- sub menu -->
+            <div class="sub-header border-bottom pl-4 pt-3 pb-3 bg-white">
+                <div class="d-flex no-block">
+                    <div class="align-self-center font-14 mr-3" style="color: rgba(79,80,80,.7);">
+                        <a href="./?view=traffic-overview" class="">Tổng quan</a>
+                    </div>
+
+                </div>
+
+            </div>
+            <!-- sub menu -->
+            <div class="row justify-content-center">
+                <!-- <div class="col-12 text-center maxWidthPc-800"> -->
+                <div class="col-12 text-center" style="max-width: 900px">
+                    <div class="py-3 py-md-5 h-100 px-3 px-md-5">
+                        <div class="font-gg font-weight-400 fontsize-32 text-center">
+                            Công Cụ Tăng Traffic
+                        </div>
+
+                        <div class="font-gg text-muted font-14 text-center mt-3 font-weight-400">
+                            Giúp bạn tăng lượng truy cập <span
+                                class="font-gg font-14 font-weight-500 text-info">Website</span> - tăng lượt view, like,
+                            share, subscribers trên <span
+                                class="font-gg font-14 font-weight-500 text-danger">Youtube</span> - tăng view, like,
+                            follow trên <span class="font-gg font-14 font-weight-500 text-dark">Tiktok</span>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-12 text-center" style="max-width: 900px">
+                    <div class="pb-5">
+                        <div class="row mb-5">
+                            <div class="col-12 col-md-4 mb-5 mb-md-0">
+                                <div class="px-4 py-3">
+                                    <div class="rounded-circle bg-info-2 shadow-sm m-auto d-flex no-block justify-content-center align-items-center"
+                                        style="width: 120px; height: 120px">
+                                        <i class="far fa-window text-info" style="font-size: 60px"></i>
+                                    </div>
+                                    <div class="mt-4 text-info fontsize-18 font-weight-500">
+                                        Traffic Website
+                                    </div>
+                                    <div class="mt-3 font-gg font-13 text-muted">
+                                        Tăng truy cập cho website giúp cải thiện thứ hạng SEO bằng cách tăng Visitor
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-4 mb-5 mb-md-0">
+                                <div class="px-4 py-3">
+                                    <div class="rounded-circle bg-danger-2 shadow-sm m-auto d-flex no-block justify-content-center align-items-center"
+                                        style="width: 120px; height: 120px">
+                                        <i class="fab fa-youtube text-danger" style="font-size:60px"></i>
+                                    </div>
+                                    <div class="mt-4 text-danger fontsize-18 font-weight-500">
+                                        Traffic Youtube
+                                    </div>
+                                    <div class="mt-3 font-gg font-13 text-muted">
+                                        Tăng truy cập cho website giúp cải thiện thứ hạng SEO bằng cách tăng Visitor
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-4">
+                                <div class="card-traffic bg-white rounded shadow-sm d-flex no-block d-md-block">
+                                    <div class="image image-3 d-flex no-block justify-content-center px-5"
+                                        style="padding: 70px 0">
+                                        <i class="fad fa-users-class align-self-center text-danger"
+                                            style="font-size: 50px"></i>
+                                    </div>
+                                    <div class="w-100 px-3 py-3">
+                                        <div class="">
+                                            <div class="mb-2">
+                                                <span
+                                                    class="position-relative text-danger font-gg font-16 font-weight-bold">
+                                                    300,000
+                                                    <span
+                                                        class="position-absolute text-danger font-gg font-10 font-weight-500"
+                                                        style="top: -5px">
+                                                        vnd
+                                                    </span>
+                                                </span>
+                                            </div>
+                                            <div class="font-gg font-weight-bold font-16 text-muted mb-3">
+                                                10,000 visitor
+                                            </div>
+                                        </div>
+                                        <div class="">
+                                            <button type="button" data-price="300000"
+                                                class="btn-buy-now font-gg btn btn-outline-danger"
+                                                style="width: 130px; border-radius: 20px">Mua ngay</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row mb-5">
+                            <div class="col-12">
+                                <div class="text-left rounded px-4 py-3">
+                                    <div class="font-gg font-weight-500 font-16">
+                                        Lưu ý :
+                                    </div>
+                                    <div class="ml-3">
+                                        <div class="font-gg font-13 font-weight-500 text-muted">
+                                            - Số lượng visitor khi chạy thực tế có thể nhiều hơn (hoặc ít hơn) 1 chút so
+                                            với số lượng bạn mua
+                                        </div>
+                                        <div class="font-gg font-13 font-weight-500 text-muted">
+                                            - Bạn có thể điều chỉnh số lượng visitor theo từng URL riêng lẻ <a
+                                                href="https://help.fff.com.vn/cong-cu-seo/cong-cu-tang-traffic-website-youtube"
+                                                class="font-weight-500">hướng dẫn sử dụng</a>
+                                        </div>
+
+                                    </div>
+                                </div>
+
+                                <hr width="50%">
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
