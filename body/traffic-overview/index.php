f5147444c2f388c0747b3329d31be66a84828aad
khanh <khanh23599@gmail.com>
2020-05-08 09:40:06 +0700

    up

--- body/traffic-overview/index.php
+++ body/traffic-overview/index.php
@@ -37,16 +37,16 @@
                         <div class="row mb-5">
                             <div class="col-12 col-md-4 mb-5 mb-md-0">
                                 <div class="px-4 py-3">
-                                    <div class="rounded-circle bg-info-2 shadow-sm m-auto d-flex no-block justify-content-center align-items-center"
-                                        style="width: 120px; height: 120px">
+                                    <div class="m-auto d-flex no-block justify-content-center align-items-center"
+                                        style="width: 100px; height: 100px">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70"
-                                            viewBox="0 0 24 24">
+                                            viewBox="0 0 24 24" style="fill: #5578eb">
                                             <path
                                                 d="M2.897 4.181c2.43-2.828 5.763-4.181 9.072-4.181 4.288 0 8.535 2.273 10.717 6.554-2.722.001-6.984 0-9.293 0-1.674.001-2.755-.037-3.926.579-1.376.724-2.415 2.067-2.777 3.644l-3.793-6.596zm5.11 7.819c0 2.2 1.789 3.99 3.988 3.99s3.988-1.79 3.988-3.99-1.789-3.991-3.988-3.991-3.988 1.791-3.988 3.991zm5.536 5.223c-2.238.666-4.858-.073-6.293-2.549-1.095-1.891-3.989-6.933-5.305-9.225-1.33 2.04-1.945 4.294-1.945 6.507 0 5.448 3.726 10.65 9.673 11.818l3.87-6.551zm2.158-9.214c1.864 1.734 2.271 4.542 1.007 6.719-.951 1.641-3.988 6.766-5.46 9.248 7.189.443 12.752-5.36 12.752-11.972 0-1.313-.22-2.66-.69-3.995h-7.609z" />
                                             </svg>
                                     </div>
-                                    <div class="mt-4 text-info fontsize-18 font-weight-500">
-                                        Traffic Website
+                                    <div class="mt-3 text-info fontsize-18 font-weight-500">
+                                        Website
                                     </div>
                                     <div class="mt-3 font-gg font-13 text-muted">
                                         Tăng truy cập cho website giúp cải thiện thứ hạng SEO bằng cách tăng Visitor
@@ -55,16 +55,16 @@
                             </div>
                             <div class="col-12 col-md-4 mb-5 mb-md-0">
                                 <div class="px-4 py-3">
-                                    <div class="rounded-circle bg-danger-2 shadow-sm m-auto d-flex no-block justify-content-center align-items-center"
-                                        style="width: 120px; height: 120px">
+                                    <div class="m-auto d-flex no-block justify-content-center align-items-center"
+                                        style="width: 100px; height: 100px">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70"
                                             viewBox="0 0 24 24" style="fill: #fd397a">
                                             <path
                                                 d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z" />
                                         </svg>
                                     </div>
-                                    <div class="mt-4 text-danger fontsize-18 font-weight-500">
-                                        Traffic Youtube
+                                    <div class="mt-3 text-danger fontsize-18 font-weight-500">
+                                        Youtube
                                     </div>
                                     <div class="mt-3 font-gg font-13 text-muted">
                                         Tăng truy cập cho website giúp cải thiện thứ hạng SEO bằng cách tăng Visitor
@@ -73,8 +73,8 @@
                             </div>
                             <div class="col-12 col-md-4">
                                 <div class="px-4 py-3">
-                                    <div class="rounded-circle bg-dark-2 shadow-sm m-auto d-flex no-block justify-content-center align-items-center"
-                                        style="width: 120px; height: 120px">
+                                    <div class="m-auto d-flex no-block justify-content-center align-items-center"
+                                        style="width: 100px; height: 100px">
                                         <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="80" height="80"
                                             viewBox="0 0 48 48" style=" fill:#000000;">
                                             <path fill="#212121" fill-rule="evenodd"
@@ -91,8 +91,8 @@
                                                 clip-rule="evenodd"></path>
                                         </svg>
                                     </div>
-                                    <div class="mt-4 text-dark fontsize-18 font-weight-500">
-                                        Traffic Tiktok
+                                    <div class="mt-3 text-dark fontsize-18 font-weight-500">
+                                        Tiktok
                                     </div>
                                     <div class="mt-3 font-gg font-13 text-muted">
                                         Tăng truy cập cho website giúp cải thiện thứ hạng SEO bằng cách tăng Visitor
