f228d741ea68ecbdc4baf356734e3623a2b422fa
v5 <vuonglechi@live.com>
2020-05-18 18:40:43 +0700

    update

--- require.php
+++ require.php
@@ -1,9 +1,9 @@
 <?php 
 $demoToken = "ZGdZVktsdE91by9qOUtndjc4MjYwTHdQeXllT3NKTS9ZUHVzdThJYTNWST06OhMNb7G48NOo6noCn1JFw0I";
 $userToken = "";
-$rootURL = "http://localhost:82";
+$rootURL = "https://webrank.vn";
 date_default_timezone_set('Asia/Ho_Chi_Minh');
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ERROR);
-$version = "1.0.1.88";   
+$version = "1.0.2.0";   
