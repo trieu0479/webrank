10a8b179f042c73c7cf1be2042c211d3e6ee618f
trungtran286 <tt2861997@gmail.com>
2020-05-19 14:13:14 +0700

    up

--- dist/js/pages/traffic-overview/select.js
+++ dist/js/pages/traffic-overview/select.js
@@ -1,34 +1,34 @@
-import obj from "./obj.js"; 
+import obj from "./obj.js";
 
 
-export default class Select { 
+export default class Select {
 
-    selectService() { 
+    selectService() {
         let service = obj.service;
 
         service.forEach((val, index) => {
             $(".select-service").append(`<option value="${val.value}">${val.name}</option>`);
-            $('.content-service').append(`<div id="${val.value}" class="des ${(index != 0) ? "d-none" : ""}"><div class="font-gg font-13">${val.war}</div><div class="font-gg font-13">${val.text1}</div><div class="font-gg font-13">${val.text2}</div><div class="font-gg font-weight-bold font-13">${val.text3}</div> <div class="font-gg font-weight-bold font-13">${val.text4}</div><div class="font-gg font-weight-bold font-13">${val.text5}</div> <div class="font-gg font-weight-bold font-13">${val.text6}</div><div class="font-gg font-weight-bold font-13">${val.text7}</div><div class="font-gg font-weight-bold font-13">${val.text8}</div><div class="font-gg font-13">${val.text9}</div><div class="font-gg font-13">${val.text10}</div><div class="font-gg font-13">${val.text11}</div></div>`)
+            $('.content-service').append(`<div id="${val.value}" class="des ${(index != 0) ? "d-none" : ""}">${val.html}</div>`)
         })
-    
-    
-    
+
+
+
         $('.select-service').select2();
         $('.select-service').on('select2:select', function(e) {
             let val = e.params.data.id;
             console.log(val)
             $('.des').addClass("d-none");
-            $(`#${val}`).removeClass("d-none"); 
+            $(`#${val}`).removeClass("d-none");
         });
     }
 
-    selectCountry() {  
+    selectCountry() {
         let country = obj.country;
 
         country.forEach(val => {
             $(".select-country").append(`<option ${(val.value == "Vietnam") ? "selected" : ""} value="${val.value}">${val.name}</option>`);
         })
-    
+
         $('.select-country').select2();
     }
 
@@ -37,7 +37,7 @@ export default class Select {
         platform.forEach(val => {
             $(".select-platform").append(`<option value="${val.value}">${val.name}</option>`);
         })
-    
+
         $('.select-platform').select2();
     }
 }
